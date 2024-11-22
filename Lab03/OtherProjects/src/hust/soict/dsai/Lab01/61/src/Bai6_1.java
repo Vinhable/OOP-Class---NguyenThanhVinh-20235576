c8b105d0b9fd4beb010fb767bfee53620d82bcf5
Vinhable <binhghethocngoquyen@gmail.com>
2024-11-22 23:32:00 +0700

    Delete Lab03 directory

--- Lab03/OtherProjects/src/hust/soict/dsai/Lab01/61/src/Bai6_1.java
+++
@@ -1,9 +0,0 @@
-import javax.swing.JOptionPane;
-public class Bai6_1 {
-    public static void main(String[] args) {
-        int option = JOptionPane.showConfirmDialog(null, "Do you want to change to the first ticket?");
-        JOptionPane.showMessageDialog(null, "You've chosen: " + (option==JOptionPane.YES_OPTION? "Yes" : "No"));
-        System.exit(0);    
-    }
-    
-}
